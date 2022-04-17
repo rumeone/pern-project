9e2c98b5b3ca70d8218acae211e52f08ac583492
rumeone <yuaroslav_29@mail.ru>
2022-04-18 01:49:45 +0700

    Start working with routes

--- server/index.js
+++ server/index.js
@@ -2,10 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const sequelize = require('./db');
-const models = require('./models/models')
+const models = require('./models/models');
+const cors = require('cors');
+const router = require('./routes/index');
+
 const PORT = process.env.PORT || 5000;
 
-
+app.use(cors());
+app.use(express.json());
+app.use('/api', router);
 
 const start = async () => {
     try {
@@ -17,6 +22,6 @@ const start = async () => {
     } catch (e) {
         console.log(e);
     }
-}
+};
 
 start();
