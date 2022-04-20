391f120dd5d345652e03f3df5907a3b1c27544f0
rumeone <yuaroslav_29@mail.ru>
2022-04-21 01:41:25 +0700

    added dependencies

--- server/index.js
+++ server/index.js
@@ -4,18 +4,25 @@ const app = express();
 const sequelize = require('./db');
 const models = require('./models/models');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const router = require('./routes/index');
+const errorHandle = require('./middleware/ErrorHandleMiddleware');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload());
 app.use('/api', router);
+
+app.use(errorHandle);
 
 const start = async () => {
     try {
         await sequelize.authenticate();
-        await sequelize.sync();
+        await sequelize.sync({ alter: true });
         app.listen(PORT, () => {
             console.log(`Server has been started on port: ${PORT}`);
         });
