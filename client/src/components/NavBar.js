7f2a0846ae47c67d29de577c9724c6f298f7ca02
rumeone <yuaroslav_29@mail.ru>
2022-05-28 16:19:56 +0700

    start working with admin

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,23 +2,31 @@ import React from 'react';
 import { useContext } from 'react';
 import {Context} from "../index";
 import {Button, Container, Nav, Navbar} from "react-bootstrap";
-import {SHOP_ROUTE} from "../utlis/consts";
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utlis/consts";
 import NavLink from "react-router-dom/es/NavLink";
 import {observer} from "mobx-react-lite";
+import {useHistory} from "react-router-dom";
 
 const NavBar = observer(() => {
     const {user} = useContext(Context);
-
+    const history = useHistory();
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
                 <NavLink style={{color:'white'}} to={SHOP_ROUTE}>КупиДевайс</NavLink>
                 {user.isAuth ?
                     <Nav className="ml-auto" style={{color: 'white'}}>
-                        <Button variant={"outline-light"}>
+                        <Button variant={"outline-light"}
+                                onClick={() => history.push(ADMIN_ROUTE)}
+                        >
                             Админ панель
                         </Button>
-                        <Button variant={"outline-light"} className = "ml-4" onClick={() => user.setIsAuth(false)}>Выйти</Button>
+                        <Button variant={"outline-light"}
+                                className = "ml-4"
+                                onClick={() => history.push(LOGIN_ROUTE)}
+                        >
+                            Выйти
+                        </Button>
                     </Nav>
                     :
                     <Nav className="ml-auto" style={{color: 'white'}}>
--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
+import bigStar from "../assets/bigStar.png"
 
 const DevicePage = () => {
+    const device =  {id: 1, name: 'Iphone 12 pro', price: 25000, rating: 5, img: 'https://proinside.net/wa-data/public/shop/img/iphone-12-pro-blue-10.jpg'};
+    const description = [
+        {id:1, title: "Оперативная память", description: "5 гб"},
+        {id:2, title: "Камера", description: "12 мп"},
+        {id:3, title: "Процессор", description: "A12"},
+        {id:4, title: "Кол-во ядер", description: "6"},
+        {id:5, title: "Аккумулятор", description: "4000"},
+    ]
     return (
-        <div>
-            DevicePage
-        </div>
+        <Container>
+            <Row>
+                <Col md={4} className="mt-3">
+                    <Image width={300} height={300} src={device.img}/>
+                </Col>
+                <Col md={4}>
+                    <Row className="d-flex flex-column align-items-center">
+                        <h2>{device.name}</h2>
+                        <div
+                            className="d-flex align-items-center justify-content-center"
+                            style={{background: `url(${bigStar}) no-repeat center center`, width:260, height:260, backgroundSize: 'cover', fontSize: 64}}>
+                            {device.rating}
+                        </div>
+                    </Row>
+                </Col>
+                <Col md={4}>
+                    <Card
+                    className="d-flex flex-column align-items-center justify-content-around"
+                    style={{width: 300, height: 300, fontSize:32, border: '5px solid lightgray'}}
+                    >
+                        <h3>От: {device.price} руб.</h3>
+                        <Button variant={"outline-dark"}>Добавить в коризну!</Button>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className="d-flex flex-column m-3">
+                <h1>Характеристики</h1>
+                {description.map((info, index) =>
+                <Row key={info.id} style={{background: index % 2 ===0 ? 'lightgray' : 'white'}}>
+                    {info.title}: {info.description}
+                </Row>
+                )}
+            </Row>
+        </Container>
     );
 };
 
