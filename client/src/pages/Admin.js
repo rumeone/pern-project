239b889192896efe7f4838f8edde203dada23fc8
rumeone <yuaroslav_29@mail.ru>
2022-06-03 02:11:33 +0700

    finished with admin page, last part left

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Container} from "react-bootstrap";
+import CreateType from "../modals/CreateType";
+import CreateBrand from "../modals/CreateBrand";
+import CreateDevice from "../modals/CreateDevice";
 
 const Admin = () => {
+    const [brandVisible, setBrandVisible] = useState(false);
+    const [typeVisible, setTypeVisible] = useState(false);
+    const [deviceVisible, setDeviceVisible] = useState(false);
+
     return (
         <Container className="d-flex flex-column">
-            <Button>Добавить тип</Button>
-            <Button>Добавить бренд</Button>
-            <Button>Добавить устройство</Button>
+            <Button
+                variant={"outline-dark"}
+                className="mt-2 p-2"
+                onClick={() => setTypeVisible(true)}
+
+            >
+                Добавить тип
+            </Button>
+            <Button
+                variant={"outline-dark"}
+                className="mt-2 p-2"
+                onClick={() => setBrandVisible(true)}
+            >
+                Добавить бренд
+            </Button>
+            <Button
+                variant={"outline-dark"}
+                className="mt-2 p-2"
+                onClick={() => setDeviceVisible(true)}
+            >
+                Добавить устройство
+            </Button>
+            <CreateType show={typeVisible} onHide={() => setTypeVisible(false)}/>
+            <CreateBrand show={brandVisible} onHide={() => setBrandVisible(false)}/>
+            <CreateDevice show={deviceVisible} onHide={() => setDeviceVisible(false)}/>
         </Container>
     );
 };
+
+
 
 export default Admin;
