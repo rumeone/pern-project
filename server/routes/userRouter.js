152ada652252a1ee670b2ef71a0f7d43e1462694
rumeone <yuaroslav_29@mail.ru>
2022-06-09 01:49:44 +0700

    micro fix

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,8 +3,7 @@ const router = new Router();
 const userController = require('../controllers/userController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-
-router.post('/registration', userController.registartion);
+router.post('/registration', userController.registration);
 router.post('/login', userController.login);
 router.get('/auth', authMiddleware, userController.check);
 
